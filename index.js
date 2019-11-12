ae83fd259e8ac388bb61c145cac1cdc9c0739069
ke1evra <dolgov.konstantin@gmail.com>
2019-11-12 16:19:50 +0300

    рабочее состояние

--- index.js
+++ index.js
@@ -1,87 +1,107 @@
 const puppeteer = require('puppeteer');
 const fs = require('fs');
 const colors = require('colors');
-const URL_LIST = 'https://bambolo.ru/goods/kostyum-soldata-8273.htm';
-const URL_LIST1 = [{
-    url: 'https://bambolo.ru/goods/kostyum-soldata-8273.htm',
-    art_number: ''
-},{
-    url: 'https://bambolo.ru/goods/detskii-kostyum-ledi-bag-4286.htm',
-    art_number: ''
-}];
+
+const createCsvWriter = require('csv-writer').createObjectCsvWriter;
+const csvWriter = createCsvWriter({
+    path: ()=>{return `./output/${+new Date()}`},
+});
+
 const batik = require('./lib/batik');
-const { wfSync} = require('./modules/common');
+const { wfSync } = require('./modules/common');
+
+let batik_bambolo = batik.reduce((acc,val)=>{
+    if(val.bambolo !== null){
+        acc.push({
+            url: val.bambolo,
+            art_number: val.art_number
+        });
+    }
+    return acc;
+},[]);
 
 
 async function openPage() {
-    const browser = await puppeteer.launch({
-        headless: true,
-        args: ['--no-sandbox', '--disable-setuid-sandbox']
-    });
-    console.log(`Браузер открыт`.cyan);
-    return await browser.newPage();
+    try {
+        const browser = await puppeteer.launch({
+            headless: true,
+            args: ['--no-sandbox', '--disable-setuid-sandbox']
+        });
+        console.log(`Браузер открыт`.cyan);
+        return await browser.newPage();
+    }catch (e) {
+        console.log('Ошибка в функции openPage');
+        console.log(e)
+    }
+
 }
 
 async function goToPage(page,url){
-    console.log(`Переходим на страницу ${url}`.cyan);
-    await page.goto(url,{
-        waitUntil: 'networkidle2'
-    });
-    return page;
+    try {
+        console.log(`Переходим на страницу ${url}`.cyan);
+        await page.goto(url,{
+            waitUntil: 'networkidle2'
+        });
+        return page;
+    }catch (e) {
+        console.log('Ошибка в функции goToPage');
+        console.log(e)
+    }
+
 }
 
-openPage().then(async (page)=>{
-
-    let batik_bambolo = batik.reduce((acc,val)=>{
-        if(val.bambolo !== null){
-            acc.push({
-                url: val.bambolo,
-                art_number: val.art_number
-            });
-        }
-        return acc;
-    },[]);
-    // console.log(JSON.stringify(batik_bambolo));
-
-    let parsedData = URL_LIST1.reduce(async (acc,val)=>{
-        await goToPage(page,val.url).then(async (page)=>{
-            console.log(`Страница посещена`.cyan);
+async function scrape(page){
+    try {
+        return await page.evaluate(()=>{
             let data = {};
-            return await page.evaluate(()=>{
-                let title = document.querySelector('#breadcrumbs > li.last > a > span');
-                let cost = document.querySelector('#rpart > div.priceblock > div.fl > div.price > span.d');
-                title ? data.title = title.innerText : null;
-                cost ? data.cost = cost.innerText.split(' ').join('')*1 : null ;
+            let title = document.querySelector('#breadcrumbs > li.last > a > span');
+            let cost = document.querySelector('#rpart > div.priceblock > div.fl > div.price > span.d');
+            title ? data.title = title.innerText : null;
+            cost ? data.cost = cost.innerText.split(' ').join('')*1 : null ;
 
-                return data;
-            }).then((data)=>{
-                console.log(`Получены данные: ${JSON.stringify(data)}`.green)
-                // wfSync(data,`./output/${+new Date()}.json`);
-                acc.push(data)
-            });
-        });
-        return acc;
-    },[]);
+            return data;
+        })
+    }catch (e) {
+        console.log('Ошибка в функции scrape');
+        console.log(e)
+    }
 
-    console.log('parserData: ',parsedData);
+}
 
-    // goToPage(page,URL_LIST).then(async (page)=>{
-    //     console.log(`Страница посещена`.cyan);
-    //     let data = {};
-    //     return await page.evaluate(()=>{
-    //         let title = document.querySelector('#breadcrumbs > li.last > a > span');
-    //         let cost = document.querySelector('#rpart > div.priceblock > div.fl > div.price > span.d');
-    //         title ? data.title = title.innerText : null;
-    //         cost ? data.cost = cost.innerText.split(' ').join('')*1 : null ;
-    //
-    //         return data;
-    //     }).then((data)=>{
-    //         console.log(`Получены данные: ${JSON.stringify(data)}`.green)
-    //         wfSync(data,`./output/${+new Date()}.json`);
-    //     });
-    // });
+async function saveResult(data){
+    try {
 
-//
-}).catch((e)=>{
-    console.log(`Ошибка \n ${e}`)
-});
+        wfSync(data,`./output/${+new Date()}.json`);
+        console.log(`Файл сохранен`.cyan);
+
+        csvWriter
+            .writeRecords(data)
+            .then(()=> console.log('The CSV file was written successfully'));
+    }catch (e) {
+        console.log('Ошибка в функции saveResult');
+        console.log(e)
+    }
+
+}
+
+async function main(){
+    let newPage = await openPage();
+    let data = [];
+
+    for(let item of batik_bambolo){
+        let loadedPage = await goToPage(newPage,item.url);
+        data.push(await scrape(loadedPage));
+        data[data.length-1].art_number = item.art_number;
+        console.log(`Получены данные: ${JSON.stringify(data)}`.green)
+    }
+    return data;
+}
+(async ()=>{
+    try{
+        let data = await main();
+        await saveResult(data)
+    }
+    catch (e) {
+        console.log(e)
+    }
+})();
